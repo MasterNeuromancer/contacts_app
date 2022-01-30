5c9d3c8004829926fd8db17206984c208a0d6c07
Neuromancer93 <itrejonumber1@gmail.com>
2022-01-29 21:49:40 -0800

    removed demo code + created and centered Contact Form Stateful Widget

---
+++ lib/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ContactForm extends StatefulWidget {
+  const ContactForm({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<ContactForm> createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text(
+          'Center of the app',
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+    );
+  }
+}
